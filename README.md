bffd6d76c3aae8147c637a46e738e42ad11e4c5e
Craig Wann and Will Swanson <will@>
2018-09-24 14:59:41 -0700

    add ratings vaules to db with seed file

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ Things you may want to cover:
 * User Stories
 
 
-As a user, I want to GET and POST reviews about travel destinations.
+  As a user, I want to GET and POST reviews about travel destinations.
 
-As a user, I want to GET reviews by country or city.
+  As a user, I want to GET reviews by country or city.
 
-As a user, I want to see the most popular travel destinations by number of reviews or by overall rating.
+  As a user, I want to see the most popular travel destinations by number of reviews or by overall rating.
 
 As a user, I want to PUT and DELETE reviews, but only if I wrote them. (Start by requiring a user_name param to match the user_name of the author on the message. You can always try authentication later.)
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
 class Seed
+  Destination.destroy_all
+  Review.destroy_all
+  User.destroy_all
+
 
   def self.begin
     seed = Seed.new
     seed.generate_users
     seed.generate_destinations
     seed.generate_reviews
+    seed.generate_ratings
   end
 
   def generate_users
@@ -22,11 +27,24 @@ class Seed
   def generate_reviews
     @user_all = User.all.ids
     @dest_all = Destination.all.ids
-      binding.pry
     @dest_all.each do |it|
       3.times do |i|
         Review.create!( body: Faker::Lorem.sentence(8), user_id: @user_all.sample, destination_id: it)
       end
+    end
+  end
+
+  def generate_ratings
+    Destination.all.each do |destination|
+      ratingz = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
+      8.times do |index|
+        @dest_rate_array = destination.ratings
+        @dest_rate_array.push ratingz.sample
+        destination.update(ratings: @dest_rate_array)
+      end
+      @dest_rate_array = destination.ratings
+      @avg_rating = @dest_rate_array.inject{ |sum, el| sum + el }.to_f / @dest_rate_array.size
+      destination.update(avg_rating: @avg_rating)
     end
   end
 end
